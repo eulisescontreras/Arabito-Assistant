6d25db2f7af34c270152cb3be5325d513a868514
eulises <eulisescontrerasrengifo1@gmail.com>
2018-01-21 03:34:00 -0400

    Login and Dashboard

---
+++ src/arabitogrill/login/LoginController.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arabitogrill.login;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ *
+ * @author eulis
+ */
+public class LoginController {
+    
+    @FXML
+    private JFXTextField username;
+    @FXML
+    private JFXPasswordField password;
+    
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event) {
+        String uname = username.getText();
+        String pword = DigestUtils.shaHex(password.getText());
+        
+        if (uname.equals("admin") && password.getText().equals("admin")) {
+            closeStage();
+            loadMain();
+        } else {
+            username.getStyleClass().add("wrong-credentials");
+            password.getStyleClass().add("wrong-credentials");
+        }
+    }
+
+    @FXML
+    private void handleCancelButtonAction(ActionEvent event) {
+        System.exit(0);
+    }
+    
+    private void closeStage() {
+        ((Stage) username.getScene().getWindow()).close();
+    }
+
+    void loadMain() {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/arabitogrill/main/main.fxml"));
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setTitle("Library Assistant");
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException ex) {
+            //Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/arabitogrill/main/MainController.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arabitogrill.main;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.effects.JFXDepthManager;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+/**
+ *
+ * @author eulis
+ */
+public class MainController  implements Initializable {
+    
+    @FXML
+    private HBox book_info;
+    @FXML
+    private HBox member_info;
+    @FXML
+    private TextField bookIDInput;
+    @FXML
+    private Text bookName;
+    @FXML
+    private Text bookAuthor;
+    @FXML
+    private Text bookStatus;
+    @FXML
+    private TextField memberIDInput;
+    @FXML
+    private Text memberName;
+    @FXML
+    private Text memberMobile;
+    @FXML
+    private JFXTextField bookID;
+    @FXML
+    private StackPane rootPane;
+    @FXML
+    private JFXHamburger hamburger;
+    @FXML
+    private JFXDrawer drawer;
+    @FXML
+    private Text memberNameHolder;
+    @FXML
+    private Text memberEmailHolder;
+    @FXML
+    private Text memberContactHolder;
+    @FXML
+    private Text bookNameHolder;
+    @FXML
+    private Text bookAuthorHolder;
+    @FXML
+    private Text bookPublisherHolder;
+    @FXML
+    private Text issueDateHolder;
+    @FXML
+    private Text numberDaysHolder;
+    @FXML
+    private Text fineInfoHolder;
+    @FXML
+    private AnchorPane rootAnchorPane;
+    @FXML
+    private JFXButton renewButton;
+    @FXML
+    private JFXButton submissionButton;
+    @FXML
+    private HBox submissionDataContainer;
+    @FXML
+    private StackPane bookInfoContainer;
+    @FXML
+    private StackPane memberInfoContainer;
+    @FXML
+    private Tab bookIssueTab;
+    @FXML
+    private Tab renewTab;
+    @FXML
+    private JFXTabPane mainTabPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        JFXDepthManager.setDepth(book_info, 1);
+        JFXDepthManager.setDepth(member_info, 1);
+        try {
+            VBox toolbar = FXMLLoader.load(getClass().getResource("/arabitogrill/main/toolbar/toolbar.fxml"));
+            drawer.setSidePane(toolbar);
+            drawer.open();
+        } catch (IOException ex) {
+            //Logger.getLogger(library.assistant.ui.main.MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @FXML
+    private void handleMenuFullScreen(ActionEvent event) {
+        Stage stage = ((Stage) rootPane.getScene().getWindow());
+        stage.setFullScreen(!stage.isFullScreen());
+    }
+    
+    @FXML
+    private void handleMenuClose(ActionEvent event) {
+        ((Stage) rootPane.getScene().getWindow()).close();
+    }
+
+}
